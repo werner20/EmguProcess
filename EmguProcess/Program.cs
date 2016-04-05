9d90fa1dbbfeacc78d6e74eb9702c97747173d51
werner20 <werner20@msu.edu>
2016-04-05 17:37:31 -0400

    Tenta

--- EmguProcess/Program.cs
+++ EmguProcess/Program.cs
@@ -256,11 +256,12 @@ namespace EmguProcess
             else
             {
                 storageloc = Path.Combine("D:/local/Temp/", "encode" + blobname);
+                //storageloc = Path.Combine("D:/home/site/wwwroot/", "encode" + blobname);
                 using (var fileStream = System.IO.File.OpenWrite(storageloc))
                 {
                     blockblob.DownloadToStream(fileStream);
                 }
-                // Reserved for an export function
+                export(storageloc, videoID, dbConn, container);
             }
         }
         static void track(string fileLocation, string xmlLocation, int videoID, ref SqlConnection dbConn)
@@ -357,32 +358,69 @@ namespace EmguProcess
             double frameRate = cap.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
             int FPS = Convert.ToInt32(frameRate);
             Size vidsize = new Size(w, h);
-            string outputVideo = fileLocation + "_temp.mpeg";
+            //string outputVideo = fileLocation + "_temp.mpeg";
+            string outputVideo = fileLocation.Split('.')[0] + "_temp1.mpg";
             double frameNum = cap.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount);
 
             int fourcc = VideoWriter.Fourcc('M', 'P', 'E', 'G');
             VideoWriter vidWriter = new VideoWriter(outputVideo, fourcc, FPS, vidsize, true);
-
-            string queryString = "SELECT editedvideoside FROM Videos WHERE VideoId=@Id";
-            SqlCommand command = new SqlCommand(queryString, dbconn);
-            command.Parameters.AddWithValue("@Id", videoID);
-            //string dataStruct = command.ExecuteReader();
-            string dataStruct = (string)command.ExecuteScalar();
-
-            List<List<Tuple<Rectangle, int>>> FrameList = new JavaScriptSerializer().Deserialize<List<List<Tuple<Rectangle, int>>>>(dataStruct);
             
-            // command.Parameters.AddWithValue("@PercentProcess", 100);
+            string queryString = "SELECT EditedVideoSide FROM Videos WHERE VideoId=@Id";
+            SqlCommand getRect = new SqlCommand(queryString, dbconn);
+            getRect.Parameters.AddWithValue("@Id", videoID);
 
+            //string dataStruct = command.ExecuteReader();
+            getRect.Connection.Open();
+            string rectStruct = (string)getRect.ExecuteScalar();
+            getRect.Connection.Close();
 
-            for (int i = 0; i < FrameList.Count(); i++)
+            queryString = "SELECT IntegerList FROM Videos WHERE VideoId=@Id";
+            SqlCommand getInt = new SqlCommand(queryString, dbconn);
+            getInt.Parameters.AddWithValue("@Id", videoID);
+
+            //string dataStruct = command.ExecuteReader();
+            getInt.Connection.Open();
+            string intStruct = (string)getInt.ExecuteScalar();
+            getInt.Connection.Close();
+            
+            queryString = "UPDATE Videos SET EditedVideoSide=@empty WHERE VideoId=@Id";
+            SqlCommand reset = new SqlCommand(queryString, dbconn);
+            reset.Parameters.AddWithValue("@empty", "");
+            reset.Parameters.AddWithValue("@Id", videoID);
+            reset.Connection.Open();
+            reset.ExecuteNonQuery();
+            reset.Connection.Close();
+
+            Pair p = new Pair();
+
+            List<List<Rectangle>> RectangleList = new JavaScriptSerializer().Deserialize<List<List<Rectangle>>>(rectStruct);
+            List<List<int>> EffectList = new JavaScriptSerializer().Deserialize<List<List<int>>>(intStruct);
+            List<List<Pair>> FramesList = new List<List<Pair>>();
+
+            for(int i = 0; i < RectangleList.Count; i++)
             {
+                List<int> effects = EffectList[i];
+                List<Pair> pairs = new List<Pair>();
+                for(int j = 0; j < RectangleList[i].Count; j++)
+                {
+                    pairs.Add(new Pair(RectangleList[i][j], effects[j]));
+                }
+                FramesList.Add(pairs);
+
+            } 
+
+            for (int i = 0; i < FramesList.Count; i++)
+            {
+
+
                 cap.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.PosFrames, i);
                 frame = cap.QueryFrame();
+
 
                 if (i % 10 == 0)
                 {
                     double percent = i / frameNum * 100;
-                    string query = "UPDATE Videos SET ProgressSide=@Percent" + " WHERE VideoId=@Id";
+                    string query = "UPDATE Videos SET EncodingProcess=@Percent" + " WHERE VideoId=@Id";
                     SqlCommand update = new SqlCommand(query, dbconn);
                     update.Parameters.AddWithValue("@Percent", percent);
                     update.Parameters.AddWithValue("@Id", videoID);
@@ -390,24 +428,26 @@ namespace EmguProcess
                     update.Connection.Open();
                     update.ExecuteNonQuery();
                     dbconn.Close();
+
+                    string strI = Convert.ToString(i);
+                   
                 }
                 if (frame != null)
-                {
+                {                    
                     Mat blurredObject = new Mat();
                     blurredObject = frame;
 
-                    foreach (Tuple<Rectangle, int> pair in FrameList[i])
+                    foreach (Pair pair in FramesList[i])
                     {
-                        Rectangle face = pair.Item1;
+                        Rectangle face = (Rectangle)pair.First;
                         Mat roi = new Mat(blurredObject, face);
                         Size size = new Size(23, 23);
-                        if (pair.Item2 == 1)
+                        if ((int)pair.Second == 1)
                         {
                             CvInvoke.GaussianBlur(roi, roi, size, 30);
                         }
-                        else if (pair.Item2 == 2)
-                        {
-                            //RectanglesMarker marker = new RectanglesMarker(;
+                        else if ((int)pair.Second == 2)
+                        {                            
                             Image<Bgr, byte> Image = new Image<Bgr, byte>(blurredObject.Bitmap);
                             Image.Draw(face, new Bgr(0, 0, 255), 3);
                             blurredObject = Image.Mat;
@@ -416,11 +456,11 @@ namespace EmguProcess
                     vidWriter.Write(blurredObject);
                 }
             }
-            string command2 = System.Web.HttpContext.Current.Server.MapPath("/bin/ffmpeg.exe");
+            string command2 = "D:/home/site/wwwroot/bin/ffmpeg.exe";
             string vidNum = Convert.ToString(videoID);
             string mp4outloc = Path.Combine("D:/local/Temp/",  vidNum + "_output.mp4");
-            //string mp4outloc = Path.Combine("D:/home/site/wwwroot/bin/", v.Title + "_output.mp4");
-            string args = "-i " + command2 + " " + mp4outloc;
+            //string mp4outloc = Path.Combine("D:/home/site/wwwroot/bin/", vidNum + "_output.mp4");
+            string args = "-i " + outputVideo + " " + mp4outloc;
             Process process = new Process();
             process.StartInfo.Arguments = args;
             process.StartInfo.FileName = command2;
@@ -434,12 +474,16 @@ namespace EmguProcess
             }
 
             string vidURI = Convert.ToString(editedVid.Uri);
-            queryString = "INSERT INTO Video VALUES (@vidTitle, @vidURI)";
+            //queryString = "INSERT INTO Videos VALUES (Title = @vidTitle,  OriginalVideo = @vidURI)";
+            queryString = "UPDATE Videos SET EncodedVideo=@vidURI, EncodingProcess=@progress WHERE VideoId=@vidId";
             SqlCommand cmd = new SqlCommand(queryString, dbconn);
-            cmd.Parameters.AddWithValue("@vidTitle", "EditedVideo#" + vidNum);
+            //cmd.Parameters.AddWithValue("@vidTitle", "EditedVideo#" + vidNum);
             cmd.Parameters.AddWithValue("@vidURI", vidURI);
-
-
+            cmd.Parameters.AddWithValue("@progress", "100");
+            cmd.Parameters.AddWithValue("@vidId", videoID);
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
 
         }
     }
